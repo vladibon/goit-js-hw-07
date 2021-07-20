1849c0c2ff7581535d213af630072bcf35b4aa0b
Vladimir Bondar <vladi.bondar@gmail.com>
2021-07-20 23:19:58 +0300

    Add task-02 solution

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
-];
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+const makeIngredientItem = ingredient => {
+  const ingredientRef = document.createElement('li');
+  ingredientRef.textContent = ingredient;
+  return ingredientRef;
+};
+
+const elements = ingredients.map(makeIngredientItem);
+ingredientsRef.append(...elements);
