a2af44799c733f192311ceb152c2582c81fbe277
Vladimir Bondar <vladi.bondar@gmail.com>
2021-07-21 23:49:04 +0300

    complete task-05

---
+++ js/task-05.js
@@ -0,0 +1,7 @@
+const inputRef = document.querySelector('#name-input');
+const outputRef = document.querySelector('#name-output');
+
+inputRef.addEventListener(
+  'input',
+  () => (outputRef.textContent = inputRef.value ? inputRef.value : 'незнакомец'),
+);
